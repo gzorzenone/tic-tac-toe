6d4d9f2662ba9fd2631dc836e2ae4881d9049242
gzorzenone <140647887+gzorzenone@users.noreply.github.com>
2023-08-22 15:04:13 -0300

    Add function that checks for a winner

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,16 @@
 let gameboard = (function() {
-  let gameboardArray = [];
+  let gameboardArray = ["", "", "", "", "", "", "", "", ""];
   let cells = document.querySelectorAll(".cell");
 
   function displayGameboard() {
     gameboardArray.forEach(function(value, index) {
       cells[index].textContent = value;
     });
+    game.checkWin(gameboardArray);
   }
 
   function addMarker(position, marker) {
-    if(gameboardArray[position] === undefined) {
+    if(gameboardArray[position] === "") {
       gameboardArray[position] = marker;
       console.log(gameboardArray);
       displayGameboard();
@@ -29,7 +30,7 @@ let gameboard = (function() {
   return {
     gameboardArray: gameboardArray,
     displayGameboard: displayGameboard,
-    addMarker: addMarker,
+    addMarker: addMarker
   };
 })();
 
@@ -41,3 +42,69 @@ let player = (marker) => {
 
 let player1 = player("X");
 let player2 = player("O");
+
+let game = (function() {
+  function getGameboard(gameboard) {
+    let row1 = gameboard.slice(0, 3);
+    let row2 = gameboard.slice(3, 6);
+    let row3 = gameboard.slice(6);
+    let rows = [row1, row2, row3];
+    let col1 = [];
+    let col2 = [];
+    let col3 = [];
+    let cols = [col1, col2, col3];
+    let diag1 = [];
+    let diag2 = [];
+    let diags = [diag1, diag2];
+
+    col1.push(gameboard[0]);
+    col1.push(gameboard[3]);
+    col1.push(gameboard[6]);
+
+    col2.push(gameboard[1]);
+    col2.push(gameboard[4]);
+    col2.push(gameboard[7]);
+
+    col3.push(gameboard[2]);
+    col3.push(gameboard[5]);
+    col3.push(gameboard[8]);
+
+    diag1.push(gameboard[0]);
+    diag1.push(gameboard[4]);
+    diag1.push(gameboard[8]);
+
+    diag2.push(gameboard[2]);
+    diag2.push(gameboard[4]);
+    diag2.push(gameboard[6]);
+
+    console.log(rows, cols, diags);
+
+    return [rows, cols, diags];
+  }
+
+  function compareArrayValues(value, index, array) {
+    return (index === 0 || value === array[index - 1]) && value !== "";
+  }
+
+  function checkWin(gameboard) {
+    let fixedGameboard = getGameboard(gameboard);
+
+    for(i = 0; i < 3; i++) {
+      if(fixedGameboard[0][i].every(compareArrayValues)) {
+        return true;
+      }
+      else if(fixedGameboard[1][i].every(compareArrayValues)) {
+        return true;
+      }
+    }
+    for(i = 0; i < 2; i++) {
+      if(fixedGameboard[2][i].every(compareArrayValues)) {
+        return true;
+      }
+    }
+  }
+
+  return {
+    checkWin
+  };
+})();
