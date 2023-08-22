510653271ea509276675d709d7ff6d74c725d7ef
gzorzenone <140647887+gzorzenone@users.noreply.github.com>
2023-08-22 12:50:46 -0300

    Add function that adds markers to the gameboard

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,43 @@
 let gameboard = (function() {
-  let gameboardArray = ["O", "X", "X", "X", "O", "O", "X", "O", "O"];
+  let gameboardArray = [];
+  let cells = document.querySelectorAll(".cell");
 
   function displayGameboard() {
-    let gameboardDisplay = document.querySelector(".gameboard");
-
-    gameboardArray.forEach(function(value) {
-      let cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.textContent = value;
-      gameboardDisplay.appendChild(cell);
+    gameboardArray.forEach(function(value, index) {
+      cells[index].textContent = value;
     });
   }
 
+  function addMarker(position, marker) {
+    if(gameboardArray[position] === undefined) {
+      gameboardArray[position] = marker;
+      console.log(gameboardArray);
+      displayGameboard();
+    }
+  }
+
+  cells.forEach((cell, index) => {
+    cell.addEventListener("click", () => {
+      addMarker(index, player1.marker);
+    });
+    cell.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      addMarker(index, player2.marker);
+    });
+  });
+
   return {
     gameboardArray: gameboardArray,
-    displayGameboard: displayGameboard
+    displayGameboard: displayGameboard,
+    addMarker: addMarker,
   };
 })();
+
+let player = (marker) => {
+  return {
+    marker
+  };
+};
+
+let player1 = player("X");
+let player2 = player("O");
